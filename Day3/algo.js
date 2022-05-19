6ff4e97accc238dadbf88ac688258068a0399a53
David Rodriguez <davidrodrvguez@gmail.com>
2022-05-19 00:48:02 -0400

    update may19

---
+++ Day3/algo.js
@@ -0,0 +1,11 @@
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+for (var i = 0; i < number.length; index++) {
+    if(numbers[i] > 0){
+        countPositive++;
+    }
+    
+}
+
+console.log("there are " + countPositives + " positive values");
